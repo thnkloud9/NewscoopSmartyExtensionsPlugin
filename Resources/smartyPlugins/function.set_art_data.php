f1d3534aff518414b76944fae3e4453f62de52bb
Mark Lewis <thnkloud9@gmail.com>
2013-12-04 12:46:19 -0300

    add kobinet specific functions

---
+++ Resources/smartyPlugins/function.set_art_data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * Get weather
+ *
+ * @param array $params
+ * @param Smarty_Internal_Template $smarty
+ * @return string
+ */
+function smarty_function_set_art_data(array $params, Smarty_Internal_Template $template)
+{
+        $art_data_array = $template->getTemplateVars('art_data_array');
+        $art_sort_array = $template->getTemplateVars('art_sort_array');
+
+        array_multisort($art_sort_array, $art_data_array);
+
+        $template->assign('art_data_array',$art_data_array);
+        $template->assign('art_sort_array',$art_sort_array);
+}
---
+++ Resources/smartyPlugins/function.set_current_article_events_date.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * Get weather
+ *
+ * @param array $params
+ * @param Smarty_Internal_Template $smarty
+ * @return string
+ */
+function smarty_function_set_current_article_events_date(array $params, Smarty_Internal_Template $template)
+{
+
+    $current_article_events_sort = $template->getTemplateVars('current_article_events_sort');
+    $current_article_events_date = $template->getTemplateVars('current_article_events_date');
+
+    array_multisort($current_article_events_sort, $current_article_events_date);
+
+    $template->assign('current_article_events_date',$current_article_events_date);
+
+}
