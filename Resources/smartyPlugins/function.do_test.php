d5c64ba1857ac94009057f082728e8feda8cf773
Mark Lewis <thnkloud9@gmail.com>
2013-12-04 13:20:48 -0300

    test function

---
+++ Resources/smartyPlugins/function.do_test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/**
+ * Get weather
+ *
+ * @param array $params
+ * @param Smarty_Internal_Template $template
+ * @return string
+ */
+function smarty_function_do_test(array $params, Smarty_Internal_Template $template)
+{
+    return "<h1>TEST</h1>";
+}
